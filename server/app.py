4e6d318d6dc335324fb28234858a3e002a679005
Laurent Dosdat <ldosdat@antidot.net>
2020-04-14 15:28:43 +0200

    NEW - web service route to update a book

--- server/app.py
+++ server/app.py
@@ -1,19 +1,22 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-
+import uuid
 
 BOOKS = [
     {
+        'id': uuid.uuid4().hex,
         'title': 'On the Road',
         'author': 'Jack Kerouac',
         'read': True
     },
     {
+        'id': uuid.uuid4().hex,
         'title': 'Harry Potter and the Philosopher\'s Stone',
         'author': 'J. K. Rowling',
         'read': False
     },
     {
+        'id': uuid.uuid4().hex,
         'title': 'Green Eggs and Ham',
         'author': 'Dr. Seuss',
         'read': True
@@ -33,17 +36,13 @@ CORS(app, resources={r'/*': {'origins': '*'}})
 
 
 # @app.route('/books', methods=['GET'])
-# def all_books():
-#     return jsonify({
-#         'status': 'success',
-#         'books': BOOKS
-#     })
 @app.route('/books', methods=['GET', 'POST'])
 def all_books():
     response_object = {'status': 'success'}
     if request.method == 'POST':
         post_data = request.get_json()
         BOOKS.append({
+            'id': uuid.uuid4().hex,
             'title': post_data.get('title'),
             'author': post_data.get('author'),
             'read': post_data.get('read')
@@ -52,6 +51,30 @@ def all_books():
     else:
         response_object['books'] = BOOKS
     return jsonify(response_object)
+
+
+@app.route('/books/<book_id>', methods=['PUT'])
+def single_book(book_id):
+    response_object = {'status': 'success'}
+    if request.method == 'PUT':
+        post_data = request.get_json()
+        remove_book(book_id)
+        BOOKS.append({
+            'id': uuid.uuid4().hex,
+            'title': post_data.get('title'),
+            'author': post_data.get('author'),
+            'read': post_data.get('read')
+        })
+        response_object['message'] = 'Book updated!'
+    return jsonify(response_object)
+
+
+def remove_book(book_id):
+    for book in BOOKS:
+        if book['id'] == book_id:
+            BOOKS.remove(book)
+            return True
+    return False
 
 
 # sanity check route
