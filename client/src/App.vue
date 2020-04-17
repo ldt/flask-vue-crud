8f2319b52c998656df00552aaf6d6f0eb648b6ff
Laurent Dosdat <ldosdat@antidot.net>
2020-04-17 18:00:48 +0200

    NEW app uses BootStrap NavBar component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,33 +1,27 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> | <router-link to="/projects">Projects</router-link> |
-      <router-link to="/books">Books</router-link>
-    </div>
+    <nav-bar />
     <router-view />
   </div>
 </template>
 
+<script>
+import NavBar from "@/components/NavBar.vue";
+
+export default {
+  components: {
+    NavBar
+  }
+};
+</script>
+
 <style>
 #app {
-  margin-top: 60px;
+  /* margin-top: 60px; */
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
